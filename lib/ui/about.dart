bbcbed688168e7c2f651d4a148b267fc9fdef8ee
keyejxptwn <keyejxptwn520@gmail.com>
2021-04-14 20:05:21 +0800

    commit about.dart

--- lib/ui/about.dart
+++ lib/ui/about.dart
@@ -10,12 +10,15 @@ class AboutPage extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.all(10.0),
         margin: EdgeInsets.all(5.0),
-        child: Expanded(
-          child: RichText(
-              text: TextSpan(
-                  text:
-                      "以上接口来自干货集中营\nhttps://gank.io/，本项目只做学习使用\n项目地址：https://github.com/keyejxptwn/GankIo 欢迎Star或指正！",
-                  style: TextStyle(color: Colors.blue, fontSize: 20.0))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Text(
+                  "以上接口来自干货集中营：https://gank.io/\n本项目只做学习使用\n项目地址：https://github.com/keyejxptwn/GankIo\n欢迎Star或指正！",
+                  style: TextStyle(color: Colors.blue, fontSize: 20.0)),
+            )
+          ],
         ),
       ),
     );
